f0e4793d8aa4698e67371660cfb6d6d2c8557786
Pavan Bejadi <pavanbejadi300@gmail.com>
2026-02-20 09:00:13 +0530

    chore: remove debug logs and secure api keys

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log("API Key Check:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
